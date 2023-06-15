006d92e1cbe4e26a9c3fe9aa5ee3239fb0da4325
krish <114145459+krishkumar84@users.noreply.github.com>
2023-06-15 18:53:50 +0530

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https"); 
+require('dotenv').config();
 
 const app = express();
 app.use(express.static("public"));
